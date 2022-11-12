748cef3fba542d928ab1a18b69ee1b93a31a8f8a
JDWdonga21 <ongame21212144@gmail.com>
2022-11-12 22:48:07 +0900

    학생정보 표현 클래스 추가 - 하위정보 : 이름 / 출생년도

---
+++ app/src/main/java/com/jdwdonga/listviewpractice_dw/datas/Student.kt
@@ -0,0 +1,10 @@
+package com.jdwdonga.listviewpractice_dw.datas
+
+// 학생의 하위 정보 - 이름, 출생연도
+
+// 생성자를 만들 때 class 이름에 ()를 넣는다. 맴버변수를 넣어준다.
+class Student(
+    val name : String,
+    val birthYear : Int) {
+
+}
