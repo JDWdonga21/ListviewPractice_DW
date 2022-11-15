ca4946b82f5daae32eec5e9f9c72a3f315f3a217
jdwdonga21 <ongame21212144@gmail.com>
2022-11-15 15:28:14 +0900

    Adapter 변수 추가 리스트뷰의 어뎁터로써 연결

--- app/src/main/java/com/jdwdonga/listviewpractice_dw/MainActivity.kt
+++ app/src/main/java/com/jdwdonga/listviewpractice_dw/MainActivity.kt
@@ -2,12 +2,17 @@ package com.jdwdonga.listviewpractice_dw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jdwdonga.listviewpractice_dw.adapters.studentAdapter
 import com.jdwdonga.listviewpractice_dw.datas.Student
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
 //    맴버변수로 선언
     val mStudentList = ArrayList<Student>()
+
+//    나중에 초기화 한다.
+    lateinit var mAdapter: studentAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +22,9 @@ class MainActivity : AppCompatActivity() {
         mStudentList.add( Student("문슬아", 1990) )
         mStudentList.add( Student("곽도영", 1985) )
         mStudentList.add( Student("윤자명", 1977) )
+
+        mAdapter = studentAdapter(this, R.layout.student_list_item, mStudentList)
+
+        studentListView.adapter = mAdapter
     }
 }
