b30b85fc93fe2d872c6dc235ea0500e515cc0007
jdwdonga21 <ongame21212144@gmail.com>
2022-11-15 15:19:10 +0900

    학생 목록을 담을 ArrayList 생성 실제 목록 추가

--- app/src/main/java/com/jdwdonga/listviewpractice_dw/MainActivity.kt
+++ app/src/main/java/com/jdwdonga/listviewpractice_dw/MainActivity.kt
@@ -2,10 +2,20 @@ package com.jdwdonga.listviewpractice_dw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jdwdonga.listviewpractice_dw.datas.Student
 
 class MainActivity : AppCompatActivity() {
+
+//    맴버변수로 선언
+    val mStudentList = ArrayList<Student>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add( Student("정동욱", 1989) )
+        mStudentList.add( Student("문슬아", 1990) )
+        mStudentList.add( Student("곽도영", 1985) )
+        mStudentList.add( Student("윤자명", 1977) )
     }
 }
